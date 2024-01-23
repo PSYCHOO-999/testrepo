ccd63189a9bff97033fdd9c12c399cc01d12eacf
Ankit Raj <140009332+PSYCHOO-999@users.noreply.github.com>
2024-01-23 22:05:33 +0530

    Update weather_report.js

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -2,7 +2,7 @@ function showweatherDetails(event) {
     event.preventDefault();
   
     const city = document.getElementById('city').value;
-    const apiKey = '0b94e6762e9339877ae7d99412ec5ffc'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiKey = 'c4f86ece00bc8aa272652ac9065af12d'; // Replace 'YOUR_API_KEY' with your actual API key
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   
     fetch(apiUrl)
